3d665114a4b72d7c49f558f9df609cec479a6c7c
Cheng Ly <cly@vcu.edu>
2023-07-26 09:06:30 -0400

    Update make_DistMeans_raw.m

--- data_analysis/make_DistMeans_raw.m
+++ data_analysis/make_DistMeans_raw.m
@@ -18,8 +18,8 @@ mnSpread_ND=abs(mean(sumSpk_Orth_ND,2)-mean(sumSpk_Ret_ND,2));
 mnSpread_Bic=abs(mean(sumSpk_Orth_Bic,2)-mean(sumSpk_Ret_Bic,2));
 mnSpread_Mus=abs(mean(sumSpk_Orth_Mus,2)-mean(sumSpk_Ret_Mus,2));
 %calc std spread
-stSpread_ND=abs(std(sumSpk_Orth_ND,0,2)-std(sumSpk_Ret_ND,0,2));
-stSpread_Bic=abs(std(sumSpk_Orth_Bic,0,2)-std(sumSpk_Ret_Bic,0,2));
-stSpread_Mus=abs(std(sumSpk_Orth_Mus,0,2)-std(sumSpk_Ret_Mus,0,2));
+stSpread_ND=abs(mean(sumSpk_Orth_ND,2)./std(sumSpk_Orth_ND,0,2)-mean(sumSpk_Ret_ND,2)./std(sumSpk_Ret_ND,0,2));
+stSpread_Bic=abs(mean(sumSpk_Orth_Bic,2)./std(sumSpk_Orth_Bic,0,2)-mean(sumSpk_Ret_Bic,2)./std(sumSpk_Ret_Bic,0,2));
+stSpread_Mus=abs(mean(sumSpk_Orth_Mus,2)./std(sumSpk_Orth_Mus,0,2)-mean(sumSpk_Ret_Mus,2)./std(sumSpk_Ret_Mus,0,2));
 
 save DistMeans_raw mnSpread_* stSp*
